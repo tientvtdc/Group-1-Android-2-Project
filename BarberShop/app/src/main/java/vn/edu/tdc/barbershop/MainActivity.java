bb8bde4a309a14261e50ad9378e8a8b899b6b6cb
Tien <tientv.63.student@fit.tdc.edu.vn>
2022-05-11 14:25:49 +0700

    update

--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/MainActivity.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/MainActivity.java
@@ -6,13 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
+import vn.edu.tdc.barbershop.fragment.HomeFragment;
+import vn.edu.tdc.barbershop.fragment.ScheduleFragment;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final int FRAGMENT_HOME = 0;
+    private static final int FRAGMENT_SCHEDULE = 1;
+
+    private int mCurrentFragment = 0;
+
     private DrawerLayout mDrawerLayout;
 
     @Override
@@ -32,21 +42,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        replaceFragment(new HomeFragment());
+        navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         switch (id) {
-            case R.id.nav_home:
+            case R.id.nav_home:{
+                if (mCurrentFragment!=FRAGMENT_HOME){
+                    replaceFragment(new HomeFragment());
+                    mCurrentFragment = FRAGMENT_HOME;
+                }
                 break;
+                }
             case R.id.nav_address:
                 break;
-            case R.id.nav_schedule:
+            case R.id.nav_schedule:{
+                if (mCurrentFragment!=FRAGMENT_SCHEDULE){
+                    replaceFragment(new ScheduleFragment());
+                    mCurrentFragment = FRAGMENT_SCHEDULE;
+                }
                 break;
-
+            }
         }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame,fragment);
+        fragmentTransaction.commit();
     }
 
     @Override
--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/fragment/HomeFragment.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/fragment/HomeFragment.java
@@ -1,2 +1,21 @@
-package vn.edu.tdc.barbershop.fragment;public class HomeFragment {
+package vn.edu.tdc.barbershop.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import vn.edu.tdc.barbershop.R;
+
+public class HomeFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
 }
