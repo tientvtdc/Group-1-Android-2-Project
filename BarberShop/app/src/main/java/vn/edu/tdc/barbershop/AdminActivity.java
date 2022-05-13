eeb0226b7fd5ce47f10e5178f292daf3e7c8060e
PhuTruongThanh <phutt.15.student@fit.tdc.edu.vn>
2022-05-14 00:12:29 +0700

    update layoutAdmin

--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/AdminActivity.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/AdminActivity.java
@@ -5,18 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.app.ActionBar;
+
 
 import com.google.firebase.database.annotations.Nullable;
 import vn.edu.tdc.barbershop.adapter.UserAdapter;
 import vn.edu.tdc.barbershop.models.User;
 import android.view.View;
+import android.widget.SpinnerAdapter;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity2 extends AppCompatActivity {
+public class AdminActivity extends AppCompatActivity {
 
     private RecyclerView rcvUser;
     private UserAdapter userAdapter;
@@ -24,7 +32,14 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_admin);
+
+        //narba
+//        androix.appcompat.app.Actionbar actionbar = getSupportActionBar();
+//
+//        actionbar.setHomeButtonEnabled(true);
+//        actionbar.setDisplayHomeAsUpEnabled(true);
+//        actionbar.setTitle("Quản lý lịch hẹn");
 
         rcvUser = findViewById(R.id.rcv_user);
         userAdapter = new UserAdapter(this);
@@ -35,6 +50,23 @@ public class MainActivity2 extends AppCompatActivity {
         userAdapter.setData(getListUser());
         rcvUser.setAdapter(userAdapter);
 
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent intent = new Intent(AdminActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onBackPressed(){
+        Intent intent = new Intent(AdminActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     // Dữ liệu giả
@@ -48,6 +80,6 @@ public class MainActivity2 extends AppCompatActivity {
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main2, container, false);
+        return inflater.inflate(R.layout.activity_admin, container, false);
     }
 }
--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/MainActivity.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/MainActivity.java
@@ -29,10 +29,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private DrawerLayout mDrawerLayout;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
