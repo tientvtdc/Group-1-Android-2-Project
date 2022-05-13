cfa3e8dd90bbe2279cdf2589c2ba81748fe9b8a6
PhuTruongThanh <phutt.15.student@fit.tdc.edu.vn>
2022-05-13 08:57:48 +0700

    update repass

---
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/adapter/UserAdapter.java
@@ -0,0 +1,76 @@
+package vn.edu.tdc.barbershop;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder>{
+
+    private Context mContext;
+//    List dùng để lưu những user
+    private List<User> mListUser;
+
+    public UserAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    //Sử lý danh sách
+    public void setData(List<User> list) {
+        this.mListUser = list;
+        notifyDataSetChanged();
+    }
+
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        Cái view này bằng với 1 cái layout tạo bên item_user(item_oder)
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        //hàm xét dữ liệu lên
+        User user = mListUser.get(position);
+        if (user == null) {
+            return;
+        }
+
+        //thêm những trường khác vào đây 5
+        holder.imgUser.setImageResource(user.getResourceId());
+        holder.tvName.setText(user.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        //Nếu mlistUser khác null thì rerurn
+        if (mListUser != null){
+            return mListUser.size();
+        }
+        return 0;
+    }
+
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+//        Thêm những trường còn lại vào đây 3
+        private ImageView imgUser;
+        private TextView tvName;
+
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+//            Thêm những trường khác vào đây 4
+            imgUser = itemView.findViewById(R.id.img_user);
+            tvName = itemView.findViewById(R.id.tv_name);
+        }
+    }
+}
---
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/models/User.java
@@ -0,0 +1,34 @@
+package vn.edu.tdc.barbershop;
+
+public class User {
+
+//Thêm những trường còn lại vào đây, alt enter 2
+    private int resourceId;
+    private String name;
+
+
+
+    public User(int resourceId, String name) {
+        this.resourceId = resourceId;
+        this.name = name;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
