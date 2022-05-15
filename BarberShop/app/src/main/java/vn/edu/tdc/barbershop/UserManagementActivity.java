dfe51e5e8d953e04150d21d3cb0a798f28ceca68
Hang Thanh Tuan <tuanht.02.student@fit.tdc.edu.vn>
2022-05-16 00:06:15 +0700

    Create user management activity file

---
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/UserManagementActivity.java
@@ -0,0 +1,13 @@
+package vn.edu.tdc.barbershop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
