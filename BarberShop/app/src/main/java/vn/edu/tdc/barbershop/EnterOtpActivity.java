0b729f85397c26f9cdd61ddd70d65888c04c4556
Hang Thanh Tuan <tuanht.02.student@fit.tdc.edu.vn>
2022-05-16 23:42:59 +0700

    Create log in with phone authentication UI and function. But struggle with SHA code.

---
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/EnterOtpActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.tdc.barbershop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterOtpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_otp);
+    }
+}
