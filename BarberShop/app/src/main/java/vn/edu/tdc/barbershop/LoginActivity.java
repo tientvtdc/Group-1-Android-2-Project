ae155ce5d801fc255a65758ee87b323a8d60397c
Tien <tientv.63.student@fit.tdc.edu.vn>
2022-05-17 01:16:20 +0700

    update

--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/LoginActivity.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/LoginActivity.java
@@ -2,13 +2,35 @@ package vn.edu.tdc.barbershop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
+    private MaterialButton buttonLogin;
+    private TextInputEditText inputPhone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_form);
+
+        inputPhone = findViewById(R.id.input_login_phone);
+
+        buttonLogin = findViewById(R.id.loginbtn);
+        buttonLogin.setOnClickListener(view -> {
+            String mobile = inputPhone.getText().toString().trim();
+            if(mobile.isEmpty() || mobile.length() < 9){
+                inputPhone.setError("Enter a valid mobile");
+                inputPhone.requestFocus();
+                return;
+            }
+            Intent intent = new Intent(this, VerifyPhoneActivity.class);
+            intent.putExtra("phone", mobile);
+            startActivity(intent);
+        });
     }
 }
