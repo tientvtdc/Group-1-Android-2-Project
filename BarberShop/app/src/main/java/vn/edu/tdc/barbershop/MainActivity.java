1b4bc0cf2e2f4e1a27dc0b697b846ae260933dc9
PhuTruongThanh <phutt.15.student@fit.tdc.edu.vn>
2022-05-18 09:28:26 +0700

    You xu ly firebase, image

--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/MainActivity.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/MainActivity.java
@@ -1,5 +1,9 @@
 package vn.edu.tdc.barbershop;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,24 +13,60 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.io.IOException;
+
+import vn.edu.tdc.barbershop.fragment.EditInformationFragment;
 import vn.edu.tdc.barbershop.fragment.HomeFragment;
 import vn.edu.tdc.barbershop.fragment.ScheduleFragment;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final int FRAGMENT_HOME = 0;
     private static final int FRAGMENT_SCHEDULE = 1;
+
+    private static final int FRAGMENT_INFORMATION = 3;
+    public static final int MY_REQUEST_CODE = 10;
 //    repass
     private static final int FRAGMENT_CHANGE_PASSWORD = 4;
 
     private int mCurrentFragment = 0;
 
     private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private ImageView imgUser;
+    final private EditInformationFragment editInformationFragment = new EditInformationFragment();
 
+
+//    Chọn ảnh từ máy
+    final private ActivityResultLauncher<Intent> mActivityReultLaucher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == RESULT_OK){
+                Intent intent = result.getData();
+                if (intent == null){
+                    return;
+                }
+                Uri uri = intent.getData();
+                editInformationFragment.setmUri(uri);
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    editInformationFragment.setBitMapImageView(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +112,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 }
                 break;
             }
+            case R.id.nav_profile:{
+                if (mCurrentFragment!=FRAGMENT_INFORMATION){
+                    replaceFragment(editInformationFragment);
+                    mCurrentFragment = FRAGMENT_INFORMATION;
+                }
+                break;
+            }
         }
         mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
@@ -88,5 +135,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (mDrawerLayout.isDrawerOpen(GravityCompat.START))
             mDrawerLayout.closeDrawer(GravityCompat.START);
         else super.onBackPressed();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_REQUEST_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                openGallery();
+            }else {
+
+            }
+        }
+    }
+
+    public void openGallery(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        mActivityReultLaucher.launch(Intent.createChooser(intent, "Chọn ảnh"));
     }
 }
--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/fragment/EditInformationFragment.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/fragment/EditInformationFragment.java
@@ -1,2 +1,148 @@
-package vn.edu.tdc.barbershop.fragment;public class EditInformationFragment {
+package vn.edu.tdc.barbershop.fragment;
+import static vn.edu.tdc.barbershop.MainActivity.MY_REQUEST_CODE;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Magnifier;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import vn.edu.tdc.barbershop.MainActivity;
+import vn.edu.tdc.barbershop.R;
+
+public class EditInformationFragment extends Fragment{
+
+    private View mView;
+    private ImageView imgUser;
+    private EditText edtName;
+    private EditText edtPhone;
+    private Button brnEditInformation;
+    private Uri mUri;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_edit_information, container, false);
+        initUi();
+        initListener();
+
+        brnEditInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickEditInformation();
+            }
+        });
+
+        return mView;
+    }
+
+    private void initUi(){
+
+        imgUser = mView.findViewById(R.id.imgUser);
+        edtName = mView.findViewById(R.id.edtName);
+        brnEditInformation = mView.findViewById(R.id.btn_edit_information);
+
+        setUserInformation();
+    }
+
+    private void onClickEditInformation (){
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null){
+            return;
+        }
+        String strEditInformation = edtName.getText().toString().trim();
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(strEditInformation)
+                .setPhotoUri(mUri)
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>(){
+            @Override
+            public void onComplete(@NonNull Task<Void> task){
+                if (task.isSuccessful()){
+                    Toast.makeText(getActivity(), "Cập nhật thông tin thành công", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+    public void setUserInformation(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null){
+            return;
+        }
+        edtName.setText(user.getDisplayName());
+        //Gdule.with(getActivity()).load(user.getPhotoUrl()).error(R.drawable.ic_profile).into(imgUser);
+
+    }
+
+    private void initListener(){
+        imgUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickRequestPermisstion();
+            }
+        });
+
+        brnEditInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickEditInformation();
+            }
+        });
+    }
+
+    private void onClickRequestPermisstion(){
+        MainActivity mainActivity = (MainActivity) getActivity();
+        if (mainActivity == null){
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            mainActivity.openGallery();
+            return;
+        }
+        if (getActivity().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            mainActivity.openGallery();
+        }else {
+            String[] permisstions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            getActivity().requestPermissions(permisstions, MY_REQUEST_CODE);
+        }
+    }
+
+    public void setBitMapImageView (Bitmap bitMapImageView){
+        imgUser.setImageBitmap(bitMapImageView);
+    }
+
+
+    public void setmUri(Uri mUri) {
+        this.mUri = mUri;
+    }
 }
