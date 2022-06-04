1bfc0a821c79cb23e004b24a4cbe6eecbb84cf34
PhuTruongThanh <phutt.15.student@fit.tdc.edu.vn>
2022-06-05 00:20:05 +0700

    Push editiformatiom

--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/fragment/ScheduleFragment.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/fragment/ScheduleFragment.java
@@ -61,6 +61,6 @@ public class ScheduleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        return inflater.inflate(R.layout.fragment_edit_information, container, false);
     }
 }
