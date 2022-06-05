eeec58e88080a3f05bf015f36897cf33020b55fc
tuanht99 <tuanht.02.student@fit.tdc.edu.vn>
2022-06-05 18:52:52 +0700

    Update row of order

--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/adapter/OrderAdapter.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/adapter/OrderAdapter.java
@@ -1,2 +1,77 @@
-package vn.edu.tdc.barbershop.adapter;public class OrderAdapter {
+package vn.edu.tdc.barbershop.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import vn.edu.tdc.barbershop.R;
+import vn.edu.tdc.barbershop.UserDetailActivity;
+import vn.edu.tdc.barbershop.entity.Order;
+import vn.edu.tdc.barbershop.entity.User;
+
+public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> {
+    private Context mContext;
+    private ArrayList<Order> mOrder;
+    public OrderAdapter(Context mContext, ArrayList<Order> mOrder){
+        this.mContext = mContext;
+        this.mOrder = mOrder;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View orderView = inflater.inflate(R.layout.layout_order_row, parent, false);
+        ViewHolder viewHolder = new ViewHolder(orderView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Order user = mOrder.get(position);
+
+        holder.
+
+        Glide.with(mContext).load(user.getImage()).into(holder.imgUser);
+        holder.tvUserName.setText(user.getName());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, UserDetailActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("object_user", user);
+                intent.putExtras(bundle);
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mOrder.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imgUser;
+        private TextView tvUserName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgUser = itemView.findViewById(R.id.img_user);
+            tvUserName= itemView.findViewById(R.id.tv_username);
+        }
+    }
 }
--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/entity/Order.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/entity/Order.java
@@ -1,2 +1,73 @@
-package vn.edu.tdc.barbershop.entity;public class Order {
+package vn.edu.tdc.barbershop.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Order implements Serializable {
+    private String id;
+    private User customer;
+    private Service service;
+    private Date timeOrder;
+    private Date timeFinish;
+    private int isFinish;
+
+    public Order() {
+    }
+
+    public Order(String id, User customer, Service service, Date timeOrder, Date timeFinish, int isFinish) {
+        this.id = id;
+        this.customer = customer;
+        this.service = service;
+        this.timeOrder = timeOrder;
+        this.timeFinish = timeFinish;
+        this.isFinish = isFinish;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public Date getTimeOrder() {
+        return timeOrder;
+    }
+
+    public void setTimeOrder(Date timeOrder) {
+        this.timeOrder = timeOrder;
+    }
+
+    public Date getTimeFinish() {
+        return timeFinish;
+    }
+
+    public void setTimeFinish(Date timeFinish) {
+        this.timeFinish = timeFinish;
+    }
+
+    public int getIsFinish() {
+        return isFinish;
+    }
+
+    public void setIsFinish(int isFinish) {
+        this.isFinish = isFinish;
+    }
 }
