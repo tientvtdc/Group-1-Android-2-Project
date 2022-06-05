9ae93f1077cc00630ba4b865b9f032317374cbe6
tuanht99 <tuanht.02.student@fit.tdc.edu.vn>
2022-06-05 23:03:42 +0700

    Push order management function update

--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/OrderDetailActivity.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/OrderDetailActivity.java
@@ -1,30 +1,22 @@
 package vn.edu.tdc.barbershop;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
 import com.google.android.material.appbar.MaterialToolbar;
-
-import vn.edu.tdc.barbershop.apis.UserAPIs;
-import vn.edu.tdc.barbershop.entity.User;
+import vn.edu.tdc.barbershop.entity.Order;
 
 public class OrderDetailActivity extends AppCompatActivity {
     MaterialToolbar btnBack;
-    AutoCompleteTextView autoCompleteTextView;
-    ImageView userImage;
-    TextView tvUserName;
-    TextView tvUserPhone;
-    Button btnSaveUser;
+    TextView tvOrderUserName;
+    TextView tvOrderUserPhone;
+    TextView tvOrderServiceName;
+    TextView tvOrderDate;
+    TextView tvOrderState;
+    TextView tvOrderIsComplete;
+    TextView tvOrderCancel;
     String TAG = "test";
 
 
@@ -34,41 +26,35 @@ public class OrderDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order_detail);
         Bundle bundle = getIntent().getExtras();
         if (bundle == null) return;
-        User user = (User) bundle.get("object_user");
+        Order order = (Order) bundle.get("object_order");
 
-        btnBack = (MaterialToolbar) findViewById(R.id.userdetail_back_button);
-        userImage = (ImageView) findViewById(R.id.img_user);
-        Glide.with(this).load(user.getImage()).into(userImage);
-        tvUserName = (TextView) findViewById(R.id.tv_userdetail_name);
-        tvUserPhone = (TextView) findViewById(R.id.tv_userdetail_phonenumber);
-        autoCompleteTextView = findViewById(R.id.autoCompleteText);
-        btnSaveUser = findViewById(R.id.btn_save_user);
+        btnBack = (MaterialToolbar) findViewById(R.id.orderdetail_back_button);
+        tvOrderUserName = (TextView) findViewById(R.id.order_detail_username);
+        tvOrderUserPhone = (TextView) findViewById(R.id.order_detail_phone_number);
+        tvOrderServiceName = (TextView) findViewById(R.id.order_detail_service_name);
+        tvOrderDate = (TextView) findViewById(R.id.order_detail_order_state);
+        tvOrderState = (TextView) findViewById(R.id.order_detail_order_state);
+        tvOrderIsComplete = (TextView) findViewById(R.id.tv_order_finish);
+        tvOrderCancel = (TextView) findViewById(R.id.tv_order_cancel);
 
-        tvUserName.setText(user.getName());
-        tvUserPhone.setText(user.getPhone());
+        tvOrderUserName.setText(order.getCustomer().getName());
+        tvOrderUserPhone.setText(order.getCustomer().getPhone());
+        tvOrderServiceName.setText(order.getService().getName());
+        tvOrderDate.setText(order.getTimeOrder().toString());
 
-        String []option = {"Người quản trị", "Người dùng", "Quản lý"};
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.option_item, option);
-        switch (user.getRole()){
+        switch (order.getIsFinish()){
             case 0:
-                autoCompleteTextView.setText(arrayAdapter.getItem(1).toString(), false);
+                tvOrderState.setText("Chưa Hoàn Thành");
                 break;
             case 1:
-                autoCompleteTextView.setText(arrayAdapter.getItem(2).toString(), false);
+                tvOrderState.setTextColor(Color.GREEN);
+                tvOrderState.setText("Hoàn Thành");
                 break;
-            default:
-                autoCompleteTextView.setText(arrayAdapter.getItem(0).toString(), false);
-                break;
+            case 2:
+                tvOrderState.setTextColor(Color.RED);
+                tvOrderState.setText("Đã Huỷ");
         }
-        autoCompleteTextView.setAdapter(arrayAdapter);
 
-        btnSaveUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog diaBox = AskOption(user);
-                diaBox.show();
-            }
-        });
 
         btnBack.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,32 +64,32 @@ public class OrderDetailActivity extends AppCompatActivity {
         });
     }
 
-    private AlertDialog AskOption(User user)
-    {
-        AlertDialog myQuittingDialogBox = new AlertDialog.Builder(this)
-                // set message, title, and icon
-                .setTitle("Lưu người dùng")
-                .setMessage("Bạn có muốn lưu người dùng hiện tại?")
-                .setIcon(R.drawable.save)
-                .setPositiveButton("Lưu", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        int position = -1;
-                        if(autoCompleteTextView.getText().toString().equals("Người quản trị")) position = 2;
-                        else if(autoCompleteTextView.getText().toString().equals("Quản lý")) position = 1;
-                        else if(autoCompleteTextView.getText().toString().equals("Người dùng")) position = 0;
-                        UserAPIs.updateUserRole(user.getId(), position != -1 ? position : user.getRole());
-                        finish();
-                    }
-                })
-                .setNegativeButton("Thoát", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                })
-                .create();
-
-        return myQuittingDialogBox;
-    }
+//    private AlertDialog AskOption(User user)
+//    {
+//        AlertDialog myQuittingDialogBox = new AlertDialog.Builder(this)
+//                // set message, title, and icon
+//                .setTitle("Lưu người dùng")
+//                .setMessage("Bạn có muốn lưu người dùng hiện tại?")
+//                .setIcon(R.drawable.save)
+//                .setPositiveButton("Lưu", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        int position = -1;
+//                        if(autoCompleteTextView.getText().toString().equals("Người quản trị")) position = 2;
+//                        else if(autoCompleteTextView.getText().toString().equals("Quản lý")) position = 1;
+//                        else if(autoCompleteTextView.getText().toString().equals("Người dùng")) position = 0;
+//                        UserAPIs.updateUserRole(user.getId(), position != -1 ? position : user.getRole());
+//                        finish();
+//                    }
+//                })
+//                .setNegativeButton("Thoát", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                    }
+//                })
+//                .create();
+//
+//        return myQuittingDialogBox;
+//    }
 }
--- BarberShop/app/src/main/java/vn/edu/tdc/barbershop/apis/OrderAPIs.java
+++ BarberShop/app/src/main/java/vn/edu/tdc/barbershop/apis/OrderAPIs.java
@@ -9,10 +9,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import vn.edu.tdc.barbershop.adapter.OrderAdapter;
 import vn.edu.tdc.barbershop.adapter.UserAdapter;
 import vn.edu.tdc.barbershop.entity.Order;
+import vn.edu.tdc.barbershop.entity.Service;
+import vn.edu.tdc.barbershop.entity.User;
 
 public class OrderAPIs {
     private static FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -24,7 +27,13 @@ public class OrderAPIs {
             public void onDataChange(DataSnapshot snapshot) {
                 mOrder.clear();
                 for (DataSnapshot postSnapshot : snapshot.getChildren()) {
-                    Order other = postSnapshot.getValue(Order.class);
+                    String id = postSnapshot.child("id").getValue(String.class);
+                    Date timeOrder = new Date(postSnapshot.child("timeOrder").getValue(Long.class));
+                    Date timeFinish = new Date(postSnapshot.child("timeFinish").getValue(Long.class));
+                    int isFinish = postSnapshot.child("finish").getValue(Integer.class);
+                    User user = postSnapshot.child("customer").getValue(User.class);
+                    Service service = postSnapshot.child("service").getValue(Service.class);
+                    Order other = new Order(id, user, service, timeOrder, timeFinish, isFinish);
                     mOrder.add(other);
                     // here you can access to name property like university.name
                 }
